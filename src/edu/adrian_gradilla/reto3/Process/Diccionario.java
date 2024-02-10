8e02b9d57d9ea00f283757d023d3de4ea1e0e62e
ardillaGM99 <adrian_gm08@hotmail.com>
2024-02-10 09:34:31 -0600

    problema 1 terminado y documentado.

--- src/edu/adrian_gradilla/reto3/Process/Diccionario.java
+++ src/edu/adrian_gradilla/reto3/Process/Diccionario.java
@@ -3,37 +3,37 @@ public class Diccionario {
 
     private final String[] palabras;
     private final String[] significados;
-
+    /* Mi primer Constructor *-* */
     public Diccionario() {
         palabras = new String[10];
         significados = new String[10];
 
-
+        /*palabras y significados del diccionario*/
         palabras[0] = "Profesor";
         significados[0] = "persona que comparte conocimientos";
         palabras[1] = "Gato";
         significados[1] = "Animal doméstico";
         palabras[2] = "Amigo";
         significados[2] ="persona que compartes un fuerte vinculo";
-        palabras[3] ="";
-        significados[3]="";
-        palabras[4]="";
-        significados[4]="";
-        palabras[5]="";
-        significados[5]="";
-        palabras[6]="";
-        significados[6]="";
-        palabras[7]="";
-        significados[7]="";
-        palabras[8]="";
-        significados[8]="";
-        palabras[9]="";
-        significados[9]="";
+        palabras[3] ="perro";
+        significados[3]="Mejor amigo del humano";
+        palabras[4]="automovil";
+        significados[4]="transporte de las personas";
+        palabras[5]="Gym";
+        significados[5]="Lugar para entrenar";
+        palabras[6]="League of legends";
+        significados[6]="videojuego que te come el alma";
+        palabras[7]="Futbol";
+        significados[7]="Deporte mas popular del mundo";
+        palabras[8]="Salsa";
+        significados[8]="Mejor estilo de baile";
+        palabras[9]="Programador";
+        significados[9]="Persona que resuelve problemas";
 
 
 
     }
-
+        /*Enlisto las palabras que tengo en el Diccionario*/
     public void enlistarPalabras() {
         System.out.println("Palabras en el diccionario:");
         for (String palabra : palabras) {
@@ -42,7 +42,7 @@ public class Diccionario {
             }
         }
     }
-
+    /*Con este metodo busco el significado de las palabras*/
     public String buscarPalabra(String palabraBuscada) {
         int indice = -1;
         for (int i = 0; i < palabras.length; i++) {
@@ -58,7 +58,7 @@ public class Diccionario {
             return STR."La palabra '\{palabraBuscada}' no existe en el diccionario.";
         }
     }
-
+    /*Aqui obtengo los detalles de las palabras del diccionario*/
     public String detalleDiccionario() {
         StringBuilder detalles = new StringBuilder();
         detalles.append("Detalle del diccionario:\n");
@@ -75,7 +75,7 @@ public class Diccionario {
         }
         return detalles.toString();
     }
-
+        /*Metodo privado para contar las palabras presentes del diccionario*/
     private int contarPalabras() {
         int contador = 0;
         for (String palabra : palabras) {
--- src/edu/adrian_gradilla/reto3/Ui/CLI.java
+++ src/edu/adrian_gradilla/reto3/Ui/CLI.java
@@ -6,25 +6,26 @@ import java.util.Scanner;
 
 public class CLI {
 
+    /*Instancia de la clase Diccionario*/
     private final Diccionario Diccionario;
-
+     /*Constructor que inicializa la Clase*/
     public CLI() {
         Diccionario = new Diccionario();
     }
-
+    /*Metodo para manejar el menu e interactuar con las opciones del usuario*/
     public void showMenu() {
         Scanner scanner = new Scanner(System.in);
 
         char opcion;
         do {
-            System.out.println("---------------------------------------");
-            System.out.println("\nMenú:");
+
+            System.out.println("\n--------Diccionario:---------------");
             System.out.println("A. Enlistar palabras del diccionario");
             System.out.println("B. Buscar palabra en el diccionario");
             System.out.println("C. Detalle del diccionario");
             System.out.println("S. Finalizar programa");
             System.out.print("Seleccione una opción: ");
-            System.out.println("\n----------------------------------------");
+            System.out.println("\n-----------------------------------");
             opcion = scanner.next().charAt(0);
 
 
@@ -42,7 +43,7 @@ public class CLI {
                     System.out.println(Diccionario.detalleDiccionario());
                     break;
                 case 'S':
-                    System.out.println("Programa finalizado.");
+                    System.out.println("---------Fin del programa------------.");
                     break;
                 default:
                     System.out.println("Opción no válida. Intente de nuevo.");
