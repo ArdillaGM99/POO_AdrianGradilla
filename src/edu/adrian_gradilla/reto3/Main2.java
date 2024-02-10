a69aaad022bbc34b93dc8228f9d9f94a24957199
ardillaGM99 <adrian_gm08@hotmail.com>
2024-02-10 13:06:08 -0600

    problema 2 terminado y documentado

---
+++ src/edu/adrian_gradilla/reto3/Main2.java
@@ -0,0 +1,10 @@
+package edu.adrian_gradilla.reto3;
+
+import edu.adrian_gradilla.reto3.Ui.CLI2;
+
+public class Main2 {
+    public static void main(String[] args) {
+        CLI2 menu = new CLI2();
+        menu.showMenu();
+    }
+}
